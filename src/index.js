8fbf3bab75061e438097dce2c32a51a5dea1e277
edofransisco011 <edofransisco011@gmail.com>
2023-06-03 12:50:16 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { SidebarProvider } from './context/sidebar_context';
+import { CoursesProvider } from './context/courses_context';
+import { CartProvider } from './context/cart_context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <SidebarProvider>
+    <CoursesProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </CoursesProvider>
+  </SidebarProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
